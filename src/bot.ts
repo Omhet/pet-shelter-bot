a99a3a090a4af360bdea39064c67121c58be5cb9
Vladimir Ivanov <omhetehmo@yandex.ru>
2020-12-24 17:56:49 +0500

    Separate files

--- src/bot.ts
+++ src/bot.ts
@@ -1,5 +1,21 @@
 import Telegraf from "telegraf";
+import { extraPhotoOptions } from "./constants";
+import { getRandomDog, getDogCaption, getRandomCat, getCatCaption } from "./utils";
 
 export const bot = new Telegraf(process.env.BOT_TOKEN!);
 bot.launch();
 console.log('Bot started');
+
+bot.start((ctx) => {
+    ctx.reply('Привет!');
+});
+bot.hears('С', async (ctx) => {
+    const animal = await getRandomDog();
+    const caption = getDogCaption(animal);
+    ctx.replyWithPhoto(animal.img, { caption, ...extraPhotoOptions });
+});
+bot.hears('К', async (ctx) => {
+    const animal = await getRandomCat();
+    const caption = getCatCaption(animal);
+    ctx.replyWithPhoto(animal.img, { caption, ...extraPhotoOptions });
+});
